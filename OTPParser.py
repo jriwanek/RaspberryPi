707b4772fbc71e15b0c05ca8042865c9e1895d74
Jasmine Iwanek <jasmine@iwanek.co.uk>
2019-01-09 14:12:30 -0800

    Update OTPParser.py

--- OTPParser.py
+++ OTPParser.py
@@ -43,9 +43,11 @@ try:
 except ImportError:
     sys.exit("OTPParser requires future! (Cant import 'builtins'")
 
+
 class TypoError(Exception):
     """TypoError exception."""
     pass
+
 
 MEMORY_SIZES = {
     '256':       '000',    # 0
@@ -57,71 +59,71 @@ MEMORY_SIZES = {
     'unknown_6': '110',    # 6
     'unknown_7': '111',    # 7
     '256/512':   'EITHER',
-    'unknown': ''
+    'unknown':   ''
 }
 MEMORY_SIZES_AS_STRING = dict((v, k) for k, v in list(MEMORY_SIZES.items()))
 
 MANUFACTURERS = {
-    'Sony UK':    '0000', # 0
-    'Egoman':     '0001', # 1
-    'Embest':     '0010', # 2
-    'Sony Japan': '0011', # 3
-    'Embest #2':  '0100', # 4
-    'Stadium':    '0101', # 5
-    'unknown_6':  '0110', # 6
-    'unknown_7':  '0111', # 7
-    'unknown_8':  '1000', # 8
-    'unknown_9':  '1001', # 9
-    'unknown_a':  '1010', # a
-    'unknown_b':  '1011', # b
-    'unknown_c':  '1100', # c
-    'unknown_d':  '1101', # d
-    'unknown_e':  '1110', # e
-    'unknown_f':  '1111', # f
-    'Qisda':      'QISD', # Correct value unknown
+    'Sony UK':    '0000',  # 0
+    'Egoman':     '0001',  # 1
+    'Embest':     '0010',  # 2
+    'Sony Japan': '0011',  # 3
+    'Embest #2':  '0100',  # 4
+    'Stadium':    '0101',  # 5
+    'unknown_6':  '0110',  # 6
+    'unknown_7':  '0111',  # 7
+    'unknown_8':  '1000',  # 8
+    'unknown_9':  '1001',  # 9
+    'unknown_a':  '1010',  # a
+    'unknown_b':  '1011',  # b
+    'unknown_c':  '1100',  # c
+    'unknown_d':  '1101',  # d
+    'unknown_e':  '1110',  # e
+    'unknown_f':  '1111',  # f
+    'Qisda':      'QISD',  # Correct value unknown
     'unknown':    ''
 }
 MANUFACTURERS_AS_STRING = dict((v, k) for k, v in list(MANUFACTURERS.items()))
 
 PROCESSORS = {
-    'BCM2835':   '0000', # 0
-    'BCM2836':   '0001', # 1
-    'BCM2837':   '0010', # 2
-    'unknown_3': '0011', # 3
-    'unknown_4': '0100', # 4
-    'unknown_5': '0101', # 5
-    'unknown_6': '0110', # 6
-    'unknown_7': '0111', # 7
-    'unknown_8': '1000', # 8
-    'unknown_9': '1001', # 9
-    'unknown_a': '1010', # a
-    'unknown_b': '1011', # b
-    'unknown_c': '1100', # c
-    'unknown_d': '1101', # d
-    'unknown_e': '1110', # e
-    'unknown_f': '1111', # f
+    'BCM2835':   '0000',  # 0
+    'BCM2836':   '0001',  # 1
+    'BCM2837':   '0010',  # 2
+    'unknown_3': '0011',  # 3
+    'unknown_4': '0100',  # 4
+    'unknown_5': '0101',  # 5
+    'unknown_6': '0110',  # 6
+    'unknown_7': '0111',  # 7
+    'unknown_8': '1000',  # 8
+    'unknown_9': '1001',  # 9
+    'unknown_a': '1010',  # a
+    'unknown_b': '1011',  # b
+    'unknown_c': '1100',  # c
+    'unknown_d': '1101',  # d
+    'unknown_e': '1110',  # e
+    'unknown_f': '1111',  # f
     'unknown':   ''
 
 }
 PROCESSORS_AS_STRING = dict((v, k) for k, v in list(PROCESSORS.items()))
 
 BOARD_TYPES = {
-    'A':         '00000000', # 0
-    'B':         '00000001', # 1
-    'A+':        '00000010', # 2
-    'B+':        '00000011', # 3
-    '2B':        '00000100', # 4
-    'Alpha':     '00000101', # 5
-    'CM1':       '00000110', # 6
-    'Unknown_7': '00000111', # 7 (Not in known use)
-    '3B':        '00001000', # 8
-    'Zero':      '00001001', # 9
-    'CM3':       '00001010', # a
-    'Unknown_b': '00001011', # b (Not in known use)
-    'Zero W':    '00001100', # c
-    '3B+':       '00001101', # d
-    '3A+':       '00001110', # e
-    'unknown_f': '00001111', # f (Not in known use)
+    'A':         '00000000',  # 0
+    'B':         '00000001',  # 1
+    'A+':        '00000010',  # 2
+    'B+':        '00000011',  # 3
+    '2B':        '00000100',  # 4
+    'Alpha':     '00000101',  # 5
+    'CM1':       '00000110',  # 6
+    'Unknown_7': '00000111',  # 7 (Not in known use)
+    '3B':        '00001000',  # 8
+    'Zero':      '00001001',  # 9
+    'CM3':       '00001010',  # a
+    'Unknown_b': '00001011',  # b (Not in known use)
+    'Zero W':    '00001100',  # c
+    '3B+':       '00001101',  # d
+    '3A+':       '00001110',  # e
+    'unknown_f': '00001111',  # f (Not in known use)
     'unknown':   ''
 }
 BOARD_TYPES_AS_STRING = dict((v, k) for k, v in list(BOARD_TYPES.items()))
@@ -143,7 +145,7 @@ BOARD_REVISIONS = {
     'unknown_d': '1101',
     'unknown_e': '1110',
     'unknown_f': '1111',
-    '2.0':       ' 2.0', # Correct Value unknown.
+    '2.0':       ' 2.0',  # Correct Value unknown.
     'unknown':   ''
 }
 BOARD_REVISIONS_AS_STRING = dict((v, k) for k, v in list(BOARD_REVISIONS.items()))
@@ -196,37 +198,37 @@ REGIONS = {
     'unknown_13':             13,
     'unknown_14':             14,
     'unknown_15':             15,
-    'unknown_16':             16, # Usually 00280000, sometimes 2428000 or 6c28000, as yet unknown
-    'bootmode':               17, # BootMode Register
-    'bootmode_copy':          18, # Backup copy of BootMode Register
-    'unknown_19':             19, # Always ffffffff in tests
-    'unknown_20':             20, # Always ffffffff in tests
-    'unknown_21':             21, # Always ffffffff in tests
-    'unknown_22':             22, # Always ffffffff in tests
-    'unknown_23':             23, # Always ffffffff in tests
-    'unknown_24':             24, # Always ffffffff in tests
-    'unknown_25':             25, # Always ffffffff in tests
-    'unknown_26':             26, # Always ffffffff in tests
-    'unknown_27':             27, # Has varying values, as yet unknown
-    'serial_number':          28, # Serial Number
-    'serial_number_inverted': 29, # Serial Number (Bitflipped)
-    'revision_number':        30, # Revision Number
-    'batch_number':           31, # Batch Number
-    'overclock':              32, # Overclock Register
+    'unknown_16':             16,  # Usually 00280000, sometimes 2428000 or 6c28000, as yet unknown
+    'bootmode':               17,  # BootMode Register
+    'bootmode_copy':          18,  # Backup copy of BootMode Register
+    'unknown_19':             19,  # Always ffffffff in tests
+    'unknown_20':             20,  # Always ffffffff in tests
+    'unknown_21':             21,  # Always ffffffff in tests
+    'unknown_22':             22,  # Always ffffffff in tests
+    'unknown_23':             23,  # Always ffffffff in tests
+    'unknown_24':             24,  # Always ffffffff in tests
+    'unknown_25':             25,  # Always ffffffff in tests
+    'unknown_26':             26,  # Always ffffffff in tests
+    'unknown_27':             27,  # Has varying values, as yet unknown
+    'serial_number':          28,  # Serial Number
+    'serial_number_inverted': 29,  # Serial Number (Bitflipped)
+    'revision_number':        30,  # Revision Number
+    'batch_number':           31,  # Batch Number
+    'overclock':              32,  # Overclock Register
     'unknown_33':             33,
     'unknown_34':             34,
     'unknown_35':             35,
-    'customer_one':           36, #
-    'customer_two':           37, #
-    'customer_three':         38, #
-    'customer_four':          39, #
-    'customer_five':          40, #
-    'customer_six':           41, #
-    'customer_seven':         42, #
-    'customer_eight':         43, #
+    'customer_one':           36,  #
+    'customer_two':           37,  #
+    'customer_three':         38,  #
+    'customer_four':          39,  #
+    'customer_five':          40,  #
+    'customer_six':           41,  #
+    'customer_seven':         42,  #
+    'customer_eight':         43,  #
     'unknown_44':             44,
-    'codec_key_one':          45, # Codec License Key #1
-    'codec_key_two':          46, # Codec License Key #2
+    'codec_key_one':          45,  # Codec License Key #1
+    'codec_key_two':          46,  # Codec License Key #2
     'unknown_47':             47,
     'unknown_48':             48,
     'unknown_49':             49,
@@ -244,20 +246,21 @@ REGIONS = {
     'unknown_61':             61,
     'unknown_62':             62,
     'unknown_63':             63,
-    'mac_address_two':        64, # MAC Address (Second part)
-    'mac_address_one':        65, # MAC Address (First part)
-    'advanced_boot':          66  # Advanced Boot Register
+    'mac_address_two':        64,  # MAC Address (Second part)
+    'mac_address_one':        65,  # MAC Address (First part)
+    'advanced_boot':          66,  # Advanced Boot Register
 }
 
 BOARD = {
-    'memory': '000',
+    'memory':        '000',
     'manufacturer': '0000',
-    'processor': '0000',
-    'type': '00000000',
-    'revision': '0000'
+    'processor':    '0000',
+    'type':     '00000000',
+    'revision':     '0000'
 }
 
 DATA = {}
+
 
 def is_hex(string):
     """Check if the string is hexidecimal.
@@ -268,85 +271,92 @@ def is_hex(string):
     # if string is long, then it is faster to check against a set
     return all(c in hex_digits for c in string)
 
+
 def unknown_16(name):
     """Handler for region 16."""
     indices = {
         'bits_0_to_15':  (16, 32),
-        'bits_16_to_23': (8, 16), # only 0x28 seen thus far
-        'bits_24_to_31': (0, 8)  # 0x24 & 0x6c seen here thus far
+        'bits_16_to_23': (8, 16),  # only 0x28 seen thus far
+        'bits_24_to_31': (0, 8)    # 0x24 & 0x6c seen here thus far
     }[name]
     __unknown_16 = get_binary('unknown_16')
-    return __unknown_16[indices[0] : indices[1]]
+    return __unknown_16[indices[0]: indices[1]]
+
 
 def bootmode(name):
     """Handler for region 17."""
     indices = {
-        'bit_0':         (31, 32), # Unknown (Gordon hinted the Pi wouldn't boot with this set)
-        'bit_1':         (30, 31), # Sets the oscillator frequency to 19.2MHz
-        'bit_2':         (29, 30), # Unknown (Gordon hinted the Pi wouldn't boot with this set)
-        'bit_3':         (28, 29), # Enables pull ups on the SDIO pins
-        'bits_4_to_18':  (13, 28), # Unknown/Unused
-        'bit_19':        (12, 13), # Enables GPIO bootmode
-        'bit_20':        (11, 12), # Sets the bank to check for GPIO bootmode
-        'bit_21':        (10, 11), # Enables booting from SD card
-        'bit_22':        (9, 10), # Sets the bank to boot from (That's what Gordon said, Unclear)
-        'bits_26_to_27': (7, 9), # Unknown/Unused
-        'bit_25':        (6, 7), # Unknown (Is set on the Compute Module 3)
-        'bits_23_to_24': (4, 6), # Unknown/Unused
-        'bit_28':        (3, 4), # Enables USB device booting
-        'bit_29':        (2, 3), # Enables USB host booting (Ethernet and Mass Storage)
-        'bits_30_31':    (0, 2)  # Unknown/Unused
+        'bit_0':         (31, 32),  # Unknown (Gordon hinted the Pi wouldn't boot with this set)
+        'bit_1':         (30, 31),  # Sets the oscillator frequency to 19.2MHz
+        'bit_2':         (29, 30),  # Unknown (Gordon hinted the Pi wouldn't boot with this set)
+        'bit_3':         (28, 29),  # Enables pull ups on the SDIO pins
+        'bits_4_to_18':  (13, 28),  # Unknown/Unused
+        'bit_19':        (12, 13),  # Enables GPIO bootmode
+        'bit_20':        (11, 12),  # Sets the bank to check for GPIO bootmode
+        'bit_21':        (10, 11),  # Enables booting from SD card
+        'bit_22':        (9, 10),   # Sets the bank to boot from (That's what Gordon said, Unclear)
+        'bits_26_to_27': (7, 9),    # Unknown/Unused
+        'bit_25':        (6, 7),    # Unknown (Is set on the Compute Module 3)
+        'bits_23_to_24': (4, 6),    # Unknown/Unused
+        'bit_28':        (3, 4),    # Enables USB device booting
+        'bit_29':        (2, 3),    # Enables USB host booting (Ethernet and Mass Storage)
+        'bits_30_31':    (0, 2)     # Unknown/Unused
     }[name]
     __bootmode = get_binary('bootmode')
-    return __bootmode[indices[0] : indices[1]]
+    return __bootmode[indices[0]: indices[1]]
+
 
 def unknown_27(name):
     """Handler for region 27."""
     indices = {
-        'bits_0_to_15':  (16, 32), # 5050 (1B, 2B 1.1), 7373 (2B 1.2), 2727(CM3), 1f1f (3B+)
+        'bits_0_to_15':  (16, 32),  # 5050 (1B, 2B 1.1), 7373 (2B 1.2), 2727(CM3), 1f1f (3B+)
         'bits_16_to_31': (0, 16)
     }[name]
     __unknown_27 = get_binary('unknown_27')
-    return __unknown_27[indices[0] : indices[1]]
+    return __unknown_27[indices[0]: indices[1]]
+
 
 def revision(name):
     """Handler for region 30."""
     indices = {
-        'legacy_board_revision': (27, 32), # Region used to store the legacy revision
-        'board_revision':        (28, 32), # Revision of the board
-        'board_type':            (20, 28), # Model of the board
-        'processor':             (16, 20), # Installed Processor
-        'manufacturer':          (12, 16), # Manufacturer of the board
-        'memory_size':           (9, 12), # Amount of RAM the board has
-        'new_flag':              (8, 9), # If set, this board uses the new versioning scheme
-        'bits_24_to_31':         (0, 8)  # Unused
+        'legacy_board_revision': (27, 32),  # Region used to store the legacy revision
+        'board_revision':        (28, 32),  # Revision of the board
+        'board_type':            (20, 28),  # Model of the board
+        'processor':             (16, 20),  # Installed Processor
+        'manufacturer':          (12, 16),  # Manufacturer of the board
+        'memory_size':           (9, 12),   # Amount of RAM the board has
+        'new_flag':              (8, 9),    # If set, this board uses the new versioning scheme
+        'bits_24_to_31':         (0, 8)     # Unused
     }[name]
     __revision = get_binary('revision_number')
-    return __revision[indices[0] : indices[1]]
+    return __revision[indices[0]: indices[1]]
+
 
 def overclock(name):
     """Handler for region 32."""
     indices = {
-        'overvolt_protection': (31, 32), # Overvolt protection bit
-        'bits_0_to_30':        (0, 31)  # Unknown/Unused
+        'overvolt_protection': (31, 32),  # Overvolt protection bit
+        'bits_0_to_30':        (0, 31)    # Unknown/Unused
     }[name]
     __overclock = get_binary('overclock')
-    return __overclock[indices[0] : indices[1]]
+    return __overclock[indices[0]: indices[1]]
+
 
 def advanced_boot(name):
     """Handler for region 66."""
     indices = {
-        'bits_0_to_6':   (25, 32), # GPIO for ETH_CLK output pin
-        'bit_7':         (24, 25), # Enable ETH_CLK output pin
-        'bits_8_to_14':  (17, 24), # GPIO for LAN_RUN output pin
-        'bit_15':        (16, 17), # Enable LAN_RUN output pin
-        'bits_16_to_23': (8, 16), # Unknown/Unused
-        'bit_24':        (7, 8), # Extend USB HUB timeout parameter
-        'bit_25':        (6, 7), # ETH_CLK Frequency (0 = 25MHz, 1 = 24MHz)
-        'bits_26_to_31': (0, 6)  # Unknown/Unused
+        'bits_0_to_6':   (25, 32),  # GPIO for ETH_CLK output pin
+        'bit_7':         (24, 25),  # Enable ETH_CLK output pin
+        'bits_8_to_14':  (17, 24),  # GPIO for LAN_RUN output pin
+        'bit_15':        (16, 17),  # Enable LAN_RUN output pin
+        'bits_16_to_23': (8, 16),   # Unknown/Unused
+        'bit_24':        (7, 8),    # Extend USB HUB timeout parameter
+        'bit_25':        (6, 7),    # ETH_CLK Frequency (0 = 25MHz, 1 = 24MHz)
+        'bits_26_to_31': (0, 6)     # Unknown/Unused
     }[name]
     __advanced_boot = get_binary('advanced_boot')
-    return __advanced_boot[indices[0] : indices[1]]
+    return __advanced_boot[indices[0]: indices[1]]
+
 
 def process_bootmode():
     """Process bootmode, Check against the backup."""
@@ -354,6 +364,7 @@ def process_bootmode():
     bootmode_copy = get_binary('bootmode_copy')
     if bootmode_primary != bootmode_copy:
         print('Bootmode fields are not the same, this is a bad thing!')
+
 
 def process_serial():
     """Process Serial, Check against Inverse Serial."""
@@ -364,6 +375,7 @@ def process_serial():
             print('Serial failed checksum!')
     except TypeError:
         sys.exit('Serial number format invalid!')
+
 
 def process_revision():
     """Process Revision, Handle depending on wether it's old or new style."""
@@ -377,6 +389,7 @@ def process_revision():
                       revision('board_type'),
                       revision('board_revision'))
 
+
 def format_mac():
     """Format MAC Address in a human readable fashion."""
     mac_part_1 = get_data('mac_address_one')
@@ -386,17 +399,20 @@ def format_mac():
         return ':'.join(mac[i:i+2] for i in range(0, 12, 2))
     return 'None'
 
+
 def process_hub_timeout(bit):
     """Return the HUB timeout."""
     if bit == '1':
         return '5 Seconds'
     return '2 Seconds'
 
+
 def process_eth_clk_frequency(bit):
     """Return the ETH_CLK frequency."""
     if bit == '1':
         return '24MHz'
     return '25MHz'
+
 
 def generate_info_legacy(bits):
     """Generate information for legacy board revision."""
@@ -410,6 +426,7 @@ def generate_info_legacy(bits):
                   BOARD_TYPES[input_dict['board_type']],
                   BOARD_REVISIONS[input_dict['board_revision']])
 
+
 def generate_info(memory_size_in, manufacturer_in, processor_in, board_type_in, board_revision_in):
     """Generate information from board revision."""
     BOARD['memory'] = memory_size_in
@@ -418,18 +435,22 @@ def generate_info(memory_size_in, manufacturer_in, processor_in, board_type_in, 
     BOARD['type'] = board_type_in
     BOARD['revision'] = board_revision_in
 
+
 def get_data(loc):
     """Get unformatted data from specified OTP region."""
     region = REGIONS[loc]
     return DATA[region]
 
+
 def get_binary(loc):
     """Get binary data from specified OTP region."""
     return format(int(get_data(loc), 16), '032b')
 
+
 def get_hex(loc):
     """Get hexidecimal data from specified OTP region."""
     return format(int(get_data(loc), 16), '#08x')
+
 
 def pretty_string(value, do_binary=True):
     """Return a pretty OTP entry."""
@@ -439,16 +460,18 @@ def pretty_string(value, do_binary=True):
     except ValueError:
         sys.exit('Failed to make the string pretty!')
 
+
 def read_otp():
     """Read OTP from specified file."""
-    if len(sys.argv) > 1: # We're given an argument on the command line
+    if len(sys.argv) > 1:  # We're given an argument on the command line
         if path.isfile(sys.argv[1]):
             with open(sys.argv[1], 'r') as file:
                 __read_otp_inner(file)
         else:
             sys.exit('Unable to open file.')
-    else: # Use stdin instead.
+    else:  # Use stdin instead.
         __read_otp_inner(sys.stdin)
+
 
 def __read_otp_inner(myfile):
     """Inner part of OTP file reader."""
@@ -473,6 +496,7 @@ def __read_otp_inner(myfile):
             sys.exit('Invalid OTP Dump')
         except TypoError:
             sys.exit("Invalid OTP Dump. Please run 'vcgencmd otp_dump' to create file.")
+
 
 read_otp()
 process_bootmode()
